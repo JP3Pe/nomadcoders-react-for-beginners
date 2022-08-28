9555e1747138204d35486bffcc64ae789473ef56
JP3Pe <rlaworhkd430430@gmail.com>
2022-08-28 15:28:18 +0900

    add: Movie app part One

--- src/App.js
+++ src/App.js
@@ -1,5 +1,47 @@
+import { useEffect, useState } from "react";
+
 function App() {
-  return <div className="App"></div>;
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  console.log(movies);
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading..</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} alt={movie.title + "image"} />
+              <h2>
+                <a href={movie.url}>{movie.title}></a>
+              </h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((genre) => (
+                  <li key={genre}>{genre}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
